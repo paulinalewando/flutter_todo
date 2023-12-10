36fe8b231178849315c8bfa93e8a587039ef14db
paulinalewand <paulina.lewandowska@amsterdamstandard.com>
2023-12-10 17:11:10 +0100

    feat: bottom nav menu

--- lib/pages/todos.dart
+++ lib/pages/todos.dart
@@ -5,7 +5,19 @@ class Todos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Todo(title: "TODO 1", description: "Do sth");
+    return ListView(
+      children: const [
+        Todo(title: "TODO 1", description: "Do sth"),
+        Todo(title: "TODO 1", description: "Do sth"),
+        Todo(title: "TODO 1", description: "Do sth"),
+        Todo(title: "TODO 1", description: "Do sth"),
+        Todo(title: "TODO 1", description: "Do sth"),
+        Todo(title: "TODO 1", description: "Do sth"),
+        Todo(title: "TODO 1", description: "Do sth"),
+        Todo(title: "TODO 1", description: "Do sth"),
+        Todo(title: "TODO 1", description: "Do sth"),
+      ],
+    );
   }
 }
 
@@ -22,24 +34,50 @@ class Todo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-              child: Text(
-            title,
-            style: Theme.of(context).textTheme.headlineMedium,
-          )),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            description,
-            style: Theme.of(context).textTheme.headlineSmall,
+    return SizedBox(
+      height: 150,
+      child: Card(
+        color: Colors.amber,
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage("images/background.png"),
+            ),
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                    child: Container(
+                  color: Colors.cyan,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      title,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ),
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Colors.redAccent,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      description,
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
