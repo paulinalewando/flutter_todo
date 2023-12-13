0ac9490c4418272cddc74a76005fc8b985721317
paulinalewand <paulina.lewandowska@amsterdamstandard.com>
2023-12-13 22:23:10 +0100

    feat: order

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -4,13 +4,75 @@ import '../models/datamodel.dart';
 
 class MenuPage extends StatelessWidget {
   final DataManager dataManager;
-  const MenuPage({super.key, required this.dataManager});
+
+  const MenuPage({Key? key, required this.dataManager}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MenuItem(
-      product: Product(id: 1, name: "product", price: 2, image: ""),
-      onAdd: () {},
+    var screenSize = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: FutureBuilder<List<Category>>(
+        future: dataManager.getMenu(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  // EACH CATEGORY STARTS HERE
+                  var category = snapshot.data![index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 32.0, bottom: 8.0, left: 8.0),
+                        child: Text(
+                          category.name,
+                          style: TextStyle(color: Colors.brown.shade400),
+                        ),
+                      ),
+                      if (screenSize.width < 500)
+                        // EACH MENU ITEM, Mobile Viewport
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: ClampingScrollPhysics(),
+                          itemCount: category.products.length,
+                          itemBuilder: (context, index) {
+                            return MenuItem(
+                              product: category.products[index],
+                              onAdd: (p) => dataManager.cartAdd(p),
+                            );
+                          },
+                        )
+                      else
+                        // EACH MENU ITEM, Large Viewport
+                        Center(
+                          child: Wrap(
+                            alignment: WrapAlignment.spaceAround,
+                            children: [
+                              for (var product in category.products)
+                                SizedBox(
+                                  width: 350,
+                                  child: MenuItem(
+                                    product: product,
+                                    onAdd: (p) => dataManager.cartAdd(p),
+                                  ),
+                                )
+                            ],
+                          ),
+                        )
+                    ],
+                  );
+                });
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          // By default, show a loading spinner.
+          return const CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
@@ -29,7 +91,7 @@ class MenuItem extends StatelessWidget {
         elevation: 4,
         child: Column(
           children: [
-            Image.asset("images/black_coffee.png"),
+            Image.network(product.image),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
